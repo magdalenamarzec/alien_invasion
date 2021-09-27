fbccb5618bba3817edd482dcb2534645967589cd
magdalenamarzec <Marzecmagdalena98@gmail.com>
2021-09-27 16:43:59 +0200

    Utworzenie pliku settings z ustawieniami okna pygame

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,18 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry"""
 
     def __init__(self):
         """Inicjalizacja gry i utworzenie jej zasobów."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja obcych")
-
-        #Zdefiniowanie koloru tła
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Rozpoczęcie pętli głównej gry."""
@@ -24,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             #Odświeżenie ekranu w trakcie każdej iteracji pętli
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             #Wyświetlenie ostatnio zmidyfikowanego ekranu.
             pygame.display.flip()
