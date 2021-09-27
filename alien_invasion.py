9d03dad82de7e737f0a93274f35e125a51fe688a
magdalenamarzec <Marzecmagdalena98@gmail.com>
2021-09-27 16:01:24 +0200

    Utworzenie okna pygame i koloru jego tła

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,35 @@
 import sys
 
 import pygame
+
+class AlienInvasion:
+    """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry"""
+
+    def __init__(self):
+        """Inicjalizacja gry i utworzenie jej zasobów."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Inwazja obcych")
+
+        #Zdefiniowanie koloru tła
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Rozpoczęcie pętli głównej gry."""
+        while True:
+            #Oczekiwanie na naciśnięcie klawisza przycisku myszy.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #Odświeżenie ekranu w trakcie każdej iteracji pętli
+            self.screen.fill(self.bg_color)
+
+            #Wyświetlenie ostatnio zmidyfikowanego ekranu.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #Utworzenie egzemplarza gry i jej uruchomienie.
+    ai = AlienInvasion()
+    ai.run_game()
